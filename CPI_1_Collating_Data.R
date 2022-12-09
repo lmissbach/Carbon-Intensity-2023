b32eb80e2cdf359e791d2783d0fc1891db7c0648
Leonard Missbach <missbach@mcc-berlin.net>
2022-12-09 11:35:56 +0100

    Add fuels

--- CPI_1_Collating_Data.R
+++ CPI_1_Collating_Data.R
@@ -277,9 +277,22 @@ for (i in Country.Set){
 Cooking.Codes.All.1 <- Cooking.Codes.All %>%
   select(Country, cooking_fuel, Cooking_Fuel, CF)%>%
   mutate(Cooking_Fuel = iconv(Cooking_Fuel, "UTF-8", "UTF-8", sub = ''))%>%
-  mutate(CF = ifelse(is.na(CF) & Cooking_Fuel %in% c("Charcoal", "3. Charcoal"), "Charcoal", CF))
-# TBA Marlene
-  #write.xlsx(., "0_Data/9_Supplementary Information/Cooking.Codes.All.xlsx")
+  Cooking.Codes.All.1 <- Cooking.Codes.All %>%
+  select(Country, cooking_fuel, Cooking_Fuel, CF)%>%
+  mutate(Cooking_Fuel = iconv(Cooking_Fuel, "UTF-8", "UTF-8", sub = ''))%>%
+  mutate(CF = ifelse(is.na(CF) & Cooking_Fuel %in% c("Charcoal", "3. Charcoal", "charcoal", "CHARCOAL", "3. CHARCOAL"), "Charcoal", CF))%>%
+  mutate(CF = ifelse(is.na(CF) & Cooking_Fuel %in% c("Electricity", "electricity", "electricity?", "Publicly-provided electricity/City Power", "Household generator", "9. Electricity", "10. Solar energy", "Electricity form public network", "Electricity from shared generator", "Electricity from private generator", "ELECTRICITY", "16. ELECTRIC", "Electricity from EUCL", "Other electricity distributors", "Solar panel", "Batteries+ Bulb", "Torch/Phone", "Rechargeable battery", "Other source of electricity", "Solar energy system", "Electricity-National grid", "Electricity- Solar", "Electricity- Personal Generator", "Electricity Community/ thermal plant", "Energía eléctrica", "Generator", "5. SOLAR", "Solar energy", "Electric", "Electricity  Community/ thermal plant"), "Electricity", CF))%>%
+  mutate(CF = ifelse(is.na(CF) & Cooking_Fuel %in% c("Mains gas?", "Bulk gas (zeppelin)?", "Gas in tube?", "Gas", "11. Bio gas", "Gobar gas", "Gas por cañería", "Bio Gas", "GAS", "City gas", "biogas", "15. PIPED NATURAL GAS", "13. BIOGAS", "Biogas", "Natural Gas", "Natural gas", "Biogas"), "Gas", CF))%>%
+  mutate(CF = ifelse(is.na(CF) & Cooking_Fuel %in% c("Gas in a carafe?", "Liquefied petroleum gas LPG", "LPG", "8. Butane / gas", "Supergás", "Gasl", "LIQUIFIED PETROLUM", "Liquid gas cylinders", "LPG 3 kg", "LPG 12 kg", "Elpiji 5.5 kg / blue gaz", "14. LPG/ COOKING GAS", "Liquified  petroleum  gas (LPG)"), "LPG", CF))%>%
+  mutate(CF = ifelse(is.na(CF) & Cooking_Fuel %in% c("kerosene / firewood / charcoal ?", "Kerosene", "7. Kerosene", "Paraffin-Stove", "Queroseno", "Paraffin", "kerosene", "Kerosine", "PARAFFIN", "1. KEROSENE", "Kerosene / firewood / charcoal?"), "Kerosene", CF))%>%
+  mutate(CF = ifelse(is.na(CF) & Cooking_Fuel %in% c("other ?", "Other", "Unknown", "None/donâ€™t cook", "Other (Specify)", "", "Does not cook", "Ninguno", "12. None", "13. Other specify", "Others","No cooking arrangement", "Don't cook at home", "OTHER (SPECIFY)", "Not stated", "OTHER(specify)", "Otro Cuál?","No cocinan","N/S","Ignorado", "18. OTHER (SPECIFY)", "ninguno no cocina", "nr", "No Cooking", "Lantern _Agatadowa_", "Other _specify_", "Ninguna", "Candle", "17. GARBAGE/PLASTIC", "Candles", "Oil Lamp", "other?", "None,No Cooking", "Other (specify)", "Other Fuel", "Other, specify", "None", "Unspecified", "No cooking"), "Unknown", CF))%>%
+  mutate(CF = ifelse(is.na(CF) & Cooking_Fuel %in% c("Firewood", "1. Collecting fire wood", "2. Purchase fire wood", "Wood", "Firewood and chips", "Leña", "firewood", "COLLECTED FIREWOOD", "PURCHASED FIREWOOD", "4. WOOD"), "Firewood", CF))%>%
+  mutate(CF = ifelse(is.na(CF) & Cooking_Fuel %in% c("Animal waste", "4. Crop residue / leaves", "5. Dung / manure", "6. Sawdust", "Crop residue","Sawdust", "Dung cake", "1Grass (reeds)", "Dried cow dung", "6. ANIMAL WASTE/DUNG","7. CROP RESIDUE/PLANT BIOMASS","8. SAW DUST", "STRAW/GRASS", "CROP RESIDUE","SAW DUST","ANIMAL WASTE", "Straw/shrubs/grass","Animal dung","Agricultural crop residue", "Dung of animals", "Wood, coal, plant-sources", "10. BIOMASS BRIQUETTE", "11. PROCESSED BIOMASS(PELLETS)/ WOODCHIPS", "Aninmal Waste", "Cow Dung"), "Other biomass", CF))%>%
+  mutate(CF = ifelse(is.na(CF) & Cooking_Fuel %in% c("Petrol", "12. ETHANOL", "Petroleum"), "Liquid fuel", CF))%>%
+  mutate(CF = ifelse(is.na(CF) & Cooking_Fuel %in% c("Coke, Coal", "briquette", "coal", "Briquettes", "9. COAL BRIQUETTE", "2. COAL/LIGNITE", "Coal"), "Coal", CF))%>%
+  mutate(CF = ifelse(CF == "Biomass", "Other biomass", CF))%>%
+  mutate(CF = ifelse(CF == "No Fuel" | CF == "Other", "Unknown", CF))%>%
+  mutate(CF = ifelse(is.na(CF) & is.na(Cooking_Fuel), "Unknown", CF))
 
 Lighting.Codes.All.1 <- Lighting.Codes.All %>%
   select(Country, lighting_fuel, Lighting_Fuel, LF)%>%
@@ -739,7 +752,11 @@ carbon_intensities_2.1 <- carbon_intensities_0 %>%
   mutate(code = countrycode(Country, origin = "country.name", destination = "iso3c"))%>%
   mutate(label  = ifelse(top_10 == 1, code, ""))%>%
   arrange(max_value)%>%
-  mutate(GTAP = factor(GTAP, levels = unique(GTAP[order(max_value, decreasing = TRUE)]), ordered = TRUE))
+  left_join(select(GTAP.Code, GTAP, Explanation), by = "GTAP")%>%
+  mutate(Explanation_0 = sub("\\:.*", "",Explanation))%>%
+  mutate(Explanation_0 = ifelse(is.na(Explanation_0), "Gas extraction, manufacture, distribution", Explanation_0))%>%
+  mutate(Explanation_1 = paste0(Explanation_0, " (", GTAP, ")"))%>%
+  mutate(GTAP_1 = factor(Explanation_1, levels = unique(Explanation_1[order(max_value, decreasing = TRUE)]), ordered = TRUE))
 
 carbon_intensities_2.1.1 <- carbon_intensities_0 %>%
   select(everything())%>%
@@ -753,19 +770,27 @@ carbon_intensities_2.1.1 <- carbon_intensities_0 %>%
   mutate(code = countrycode(Country, origin = "country.name", destination = "iso3c"))%>%
   mutate(label  = ifelse(top_10 == 1, code, ""))%>%
   arrange(max_value)%>%
-  mutate(GTAP = factor(GTAP, levels = unique(GTAP[order(max_value, decreasing = TRUE)]), ordered = TRUE))
+  left_join(select(GTAP.Code, GTAP, Explanation), by = "GTAP")%>%
+  mutate(Explanation_0 = sub("\\:.*", "",Explanation))%>%
+  mutate(Explanation_0 = ifelse(is.na(Explanation_0), "Gas extraction, manufacture, distribution", Explanation_0))%>%
+  mutate(Explanation_1 = paste0(Explanation_0, " (", GTAP, ")"))%>%
+  mutate(GTAP_1 = factor(Explanation_1, levels = unique(Explanation_1[order(max_value, decreasing = TRUE)]), ordered = TRUE))
   
 P_2.1 <- ggplot(carbon_intensities_2.1)+
-  geom_point(aes(y = CO2_t_per_dollar_national, x = 1), position = position_jitter(width = 0.4, seed = 2022), size = 1)+
+  geom_point(aes(y = CO2_t_per_dollar_national, x = 1, fill = factor(included)), position = position_jitter(width = 0.4, seed = 2022), size = 1.5, shape = 21)+
   geom_text_repel(aes(x = 1, label = label, y = CO2_t_per_dollar_national), 
                   size = 2.5, segment.size = 0.3, max.overlaps = Inf, position = position_jitter(width = 0.4, seed = 2022))+
-  facet_wrap(. ~ GTAP, scales = "free_y")+
+  facet_wrap(. ~ GTAP_1, scales = "free_y", labeller = label_wrap_gen(width = 35))+
   theme_bw()+
+  scale_fill_nejm(labels = c("No item matched", "Items matched"))+
+  scale_y_continuous(labels = function(x) format(x, scientific = TRUE))+
+  labs(fill = "")+
   ylab(expression(paste("Carbon intensity in t", CO[2], sep = " per USD")))+
+  xlab("")+
   ggtitle("Sectoral carbon intensity")+
   theme(axis.text.y = element_text(size = 6), 
         axis.text.x = element_blank(),
-        axis.ticks.y = element_blank(),
+        axis.ticks.x = element_blank(),
         axis.title  = element_text(size = 6),
         plot.title  = element_text(size = 10),
         legend.position = "bottom",
@@ -776,20 +801,24 @@ P_2.1 <- ggplot(carbon_intensities_2.1)+
         axis.ticks = element_line(size = 0.2),
         legend.text = element_text(size = 7),
         legend.title = element_text(size = 7),
-        plot.margin = unit(c(0.1,0.1,0,0), "cm"),
+        plot.margin = unit(c(0.3,0.3,0.3,0.3), "cm"),
         panel.border = element_rect(size = 0.3))
 
 P_2.1.1 <- ggplot(carbon_intensities_2.1.1)+
-  geom_point(aes(y = CO2_t_per_dollar_national, x = 1), position = position_jitter(width = 0.4, seed = 2022), size = 1)+
+  geom_point(aes(y = CO2_t_per_dollar_national, x = 1, fill = factor(included)), position = position_jitter(width = 0.4, seed = 2022), size = 1.5, shape = 21)+
   geom_text_repel(aes(x = 1, label = label, y = CO2_t_per_dollar_national), 
                   size = 2.5, segment.size = 0.3, max.overlaps = Inf, position = position_jitter(width = 0.4, seed = 2022))+
-  facet_wrap(. ~ GTAP, scales = "free_y")+
+  facet_wrap(. ~ GTAP_1, scales = "free_y", labeller = label_wrap_gen(width = 30))+
   theme_bw()+
+  scale_fill_manual(values = "#0072B5FF", guide = "none")+
+  scale_y_continuous(labels = function(x) format(x, scientific = TRUE))+
+  labs(fill = "")+
   ylab(expression(paste("Carbon intensity in t", CO[2], sep = " per USD")))+
+  xlab("")+
   ggtitle("Sectoral carbon intensity - included items")+
   theme(axis.text.y = element_text(size = 6), 
         axis.text.x = element_blank(),
-        axis.ticks.y = element_blank(),
+        axis.ticks.x = element_blank(),
         axis.title  = element_text(size = 6),
         plot.title  = element_text(size = 10),
         legend.position = "bottom",
@@ -800,14 +829,13 @@ P_2.1.1 <- ggplot(carbon_intensities_2.1.1)+
         axis.ticks = element_line(size = 0.2),
         legend.text = element_text(size = 7),
         legend.title = element_text(size = 7),
-        plot.margin = unit(c(0.1,0.1,0,0), "cm"),
+        plot.margin = unit(c(0.3,0.3,0.3,0.3), "cm"),
         panel.border = element_rect(size = 0.3))
 
-jpeg("1_Figures/Figure_2.1.jpg", width = 40, height = 40, unit = "cm", res = 400)
+jpeg("1_Figures/Figure_2.1.0.jpg", width = 40, height = 40, unit = "cm", res = 400)
 print(P_2.1)
 dev.off()
 
 jpeg("1_Figures/Figure_2.1.1.jpg", width = 40, height = 40, unit = "cm", res = 400)
 print(P_2.1.1)
 dev.off()
-
